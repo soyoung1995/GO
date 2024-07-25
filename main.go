46611f56fdeef7969659e2018b92c4133991375d
Your Name <you@example.com>
2024-07-25 11:37:56 +0800

    add mian.go

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
-import "fmt"
- 
-func main() {
-  var a int = 20
-  var ip *int
- 
-  ip = &a
- 
-  fmt.Print("1, Address of a variable: %x\n", &a)
-  fmt.Printf(" 2, Address stored in ip variable: %x\n", *ip )
-  fmt.Printf("3, Value of *ip variable: %d\n", *ip )
 
-                        
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, 你访问的URL路径是 %s", r.URL.Path)
+}
+
+func main() {
+	http.HandleFunc("/", handler)  // 对根路径的请求进行处理
+	http.ListenAndServe(":8888", nil) // 在8080端口启动服务
+}
